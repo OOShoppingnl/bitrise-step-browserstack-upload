4b8864960a91a4d20ac1479ccf474e0fe06463c0
Sebastiaan Eekhof <sebastiaan.eekhof@hatsa.com>
2019-10-30 11:41:16 +0100

    Create browserstack plugin

---
+++ step.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+set -x
+curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/$browserstack_endpoint/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | envman add --key BROWSERSTACK_APP_URL
